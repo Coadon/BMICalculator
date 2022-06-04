210cd1a1ad66cd0b627504b0b04f05e212f87344
Coadon <50821025+Coadon@users.noreply.github.com>
2022-06-04 17:27:25 +0800

    Initial commit

---
+++ sitescript.js
@@ -0,0 +1,9 @@
+function calculate() {
+    var height = document.getElementById("field_height").value;
+    var mass = document.getElementById("field_mass").value;
+    var result = document.getElementById("result");
+
+    var bmi = mass/Math.pow(height, 2);
+
+    result.textContent = "Your body mass index is" + bmi + " kg/m²";
+}
