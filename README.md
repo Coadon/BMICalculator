0babc37a55ac740203e10100fc4559deeb3e6f07
Coadon <50821025+Coadon@users.noreply.github.com>
2022-06-05 18:32:07 +0800

    License notice

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ An online body mass index calculator.
 
 ## Privacy Policy
 
-This website is open source, therefore, it is also non-profit.
 Any information you inserted into the calculator will only be used for calculations only. We will not use any data in any bad way.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
--- sitescript.js
+++ sitescript.js
@@ -1,3 +1,21 @@
+/*
+    BMI Calculator
+    Copyright (C) 2022  Coadon.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 function calculate() {
     var height = document.getElementById("field_height").value;
     var mass = document.getElementById("field_mass").value;
